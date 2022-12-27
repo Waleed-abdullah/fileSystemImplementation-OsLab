6070f4ed203cf36b8606af2d30ec95f2b281b111
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2022-12-27 21:42:00 +0500

    Clean up client.py

--- client.py
+++ client.py
@@ -1,24 +1,21 @@
 import socket
 
+host = '127.0.0.1'  # as both code is running on same pc
+port = 95  # socket server port number
 
 def client_program():
-    host = '127.0.0.1'  # as both code is running on same pc
-    port = 95  # socket server port number
 
     client_socket = socket.socket()  # instantiate
-
     client_socket.connect((host, port))  # connect to the server
+    
     # username exchange
     data = client_socket.recv(2048).decode()
     print(data)
+
     message = input(" -> ")  # take input
-    client_socket.send(message.encode())
-    print(client_socket.recv(2048).decode())
-
-    data = client_socket.recv(2048).decode()
-    message = input(data)
-
-    while message.lower().strip() != 'exit':
+    
+    cond = True
+    while cond:
         client_socket.send(message.encode())  # send message
         response = client_socket.recv(2048).decode()  # receive response
 
@@ -26,6 +23,8 @@ def client_program():
 
         data = client_socket.recv(2048).decode()
         message = input(data)  # again take input
+
+        cond = message.lower().strip() != 'exit'
 
     client_socket.close()  # close the connection
 
